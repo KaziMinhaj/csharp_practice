4c143e12f77dc8c25354278b7c21d030e51484e2
Kazi Md Minhajul Haider <kaziminhajulhaider@gmail.com>
2022-08-24 15:48:23 +0600

    crud operation using ado.net

---
+++ codes/ADO DOT NET/DataHelper.cs
@@ -0,0 +1,85 @@
+﻿namespace ADO_DOT_NET;
+
+using System.Data.SqlClient;
+
+public class DataHelper
+{
+    private readonly string _connectionString;
+    public DataHelper(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    private SqlCommand CreateSqlCommand(string sql, Dictionary<string, object> parameters)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+        connection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+        foreach (var parameter in parameters)
+        {
+            sqlCommand.Parameters.Add(new SqlParameter(parameter.Key, parameter.Value));
+        }
+
+        return sqlCommand;
+    }
+
+    //write operation
+    public void WriteOperation(string sql, Dictionary<string, object> parameters)
+    {
+        using SqlCommand sqlCommand = CreateSqlCommand(sql, parameters);
+        sqlCommand.ExecuteNonQuery();
+    }
+
+    //delete operation
+    public void DeleteOperation(string sql)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+
+        connection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+        sqlCommand.ExecuteNonQuery();
+    }
+
+    //update operation
+    public void UpdateOperation(string sql)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+
+        connection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+        sqlCommand.ExecuteNonQuery();
+    }
+
+    //read operation
+    public List<Dictionary<string, object>> ReadOperation(string sql)
+    {
+        SqlConnection connection = new SqlConnection(_connectionString);
+
+        connection.Open();
+
+        SqlCommand sqlCommand = new SqlCommand(sql, connection);
+
+        using SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+
+        while (reader.Read())
+        {
+            Dictionary<string, object> row = new Dictionary<string, object>();
+
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                row.Add(reader.GetName(i), reader.GetValue(i));
+            }
+            rows.Add(row);
+        }
+
+        return rows;
+    }
+}
---
+++ codes/ADO DOT NET/Program.cs
@@ -0,0 +1,45 @@
+﻿using ADO_DOT_NET;
+
+string _connectionString = @"Server=LAPTOP-88GU3220\SQLEXPRESS08;Database=CsharpB11;Trusted_Connection=True";
+
+DataHelper dataHelper = new DataHelper(_connectionString);
+
+//string name = Console.ReadLine();
+//decimal cgpa = decimal.Parse(Console.ReadLine());
+//DateTime dob = DateTime.Parse(Console.ReadLine());
+//Guid sid = Guid.Parse(Console.ReadLine());
+//bool isActive = bool.Parse(Console.ReadLine());
+
+//Dictionary<string, object> parameters = new Dictionary<string, object>();
+
+//parameters.Add("Name", name);
+//parameters.Add("Cgpa", cgpa);
+//parameters.Add("DateOfBirth", dob);
+//parameters.Add("StudentId", sid);
+//parameters.Add("IsActive", isActive);
+
+//string sqlInsert = "insert into student([name], cgpa, dateofbirth, studentid, isactive) values(@Name, @Cgpa, @DateOfBirth, @StudentId, @IsActive)";
+string sqlUpdate = "update student set name = 'Rafiq', Cgpa = 4.0 where id = 2";
+//string sqlDelete = "delete from student where id = 3";
+//string sqlRead = "select * from student";
+
+//dataHelper.WriteOperation(sqlInsert, parameters);
+
+dataHelper.UpdateOperation(sqlUpdate);
+
+//dataHelper.DeleteOperation(sqlDelete);
+
+//var data = dataHelper.ReadOperation(sqlRead);
+
+//foreach(Dictionary<string, object> item in data)
+//{
+//    foreach(var key in item.Keys)
+//    {
+//        Console.Write($"{key}:{item[key]}, " );
+//    }
+//    Console.WriteLine();
+//}
+
+
+Console.WriteLine("Complete");
+
